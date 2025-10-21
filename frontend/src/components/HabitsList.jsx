a3d1eec8de1b43842c2207fd4607a77b5c6ceef8
Thiago Augusto <luizthiagoaugusto@gmail.com>
2025-10-21 18:03:39 -0300

    Function and response to the List in Habists.jsx

--- frontend/src/components/HabitsList.jsx
+++ frontend/src/components/HabitsList.jsx
@@ -4,32 +4,61 @@ import { api } from "../api";
 
 export default function HabitsList () {
     const [habits, setHabits] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [newHabit, setNewHabit] = useState('');
 
-    useEffect(() => {
-        const fetchHabits = async () => {
-            const token = localStorage.getItem('token');
-            try {
-                const res = await api.get('/habits?userId=1', { //Substituir pelo jwt
-                    headers: {Authorization: `Bearer ${token}`}
-                    
-                });
-                setHabits(res.data);
-            } catch (error) {
-                alert('Erro ao buscar hábitos: ' + (error.response?.data?.message || error.message));
-            } 
-        };
-            
+
+
+    //Listar hábitos ao carregar o componente
+    useEffect(() => {      
         fetchHabits();
     }, []);
+
+    const fetchHabits = async () => {
+        try {
+            const res = await api.get('/habits?userId=1');
+            setHabits(res.data);
+        } catch (error) {
+            console.error("Erro ao buscar hábitos:", error);
+        }
+    };
+
+
+
+    //Adicionar novo hábito
+    const addHabit = async () => {
+        if (!newHabit) return;
+
+        try {
+            await api.post('/habits', {name: newHabit, userId: 1});
+            setNewHabit('');
+            fetchHabits();
+        } catch (error) {
+            console.error("Erro ao adicionar hábito:", error);
+        }
+    };
+
+
+    //Deletar hábito
+    const deleteHabit = async (habitId) => {
+        try {
+            await api.delete(`/habits/${habitId}`);
+            fetchHabits();
+        } catch (error) {
+            console.error("Erro ao deletar hábito:", error);
+        }
+    };
+
 
         return (
             <div>
                 <h2>Meus Hábitos</h2>
+                <input type="text" placeholder="Novo Habito" name="Habits" value={newHabit} onChange={(e) => setNewHabit(e.target.value)} />
+                <button onClick={addHabit}>Adicionar Hábito</button>
                 <ul>
-                    {habits.map(habit => (
-                        <li key={habit.totle}>
-                            {habit.description}
+                    {habits.map((habit) => (
+                        <li key={habit.id}>
+                            <span>{habit.title}</span>
+                            <button onClick={() => deleteHabit(habit.id)}>Deletar</button>
                         </li>
                     ))}
                 </ul>
